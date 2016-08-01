b596194b75ce8f20a1c1fae21d065e7ede2425f7
Samuel A. Falvo II <kc5tja@arrl.net>
2016-08-01 09:34:34 -0700

    Replace hand-derived logic with cases

--- rtl/verilog/decode_op_imm.v
+++ rtl/verilog/decode_op_imm.v
@@ -5,91 +5,437 @@
 // other functional units in the CPU.
 
 module decode_op_imm(
-	output		defined_o,
-	output		alua_rf_o,
-	output		alub_imm6i_o,
-	output		alub_imm12_o,
-	output		ra_ir1_o,
-	output		ra_ird_o,
-	output		rf_alu_o,
-	output	[2:0]	nstate_o,
-	output	[3:0]	rmask_o,
-	output		cflag_1_o,
-	output		sum_en_o,
-	output		and_en_o,
-	output		xor_en_o,
-	output		invB_en_o,
-	output		lsh_en_o,
-	output		rsh_en_o,
-	output		ltu_en_o,
-	output		lts_en_o,
-	output		sx32_en_o,
+        output          defined_o,
+        output          alua_rf_o,
+        output          alub_imm6i_o,
+        output          alub_imm12_o,
+        output          ra_ir1_o,
+        output          ra_ird_o,
+        output          rf_alu_o,
+        output  [2:0]   nstate_o,
+        output  [3:0]   rmask_o,
+        output          cflag_1_o,
+        output          sum_en_o,
+        output          and_en_o,
+        output          xor_en_o,
+        output          invB_en_o,
+        output          lsh_en_o,
+        output          rsh_en_o,
+        output          ltu_en_o,
+        output          lts_en_o,
+        output          sx32_en_o,
 
-	input	[2:0]	cstate_i,
-	input	[31:0]	ir_i
+        input   [2:0]   cstate_i,
+        input   [31:0]  ir_i
 );
-	wire s0 = (cstate_i == 3'b000);
-	wire s1 = (cstate_i == 3'b001);
-	wire s2 = (cstate_i == 3'b010);
+  wire s0 = (cstate_i == 3'b000);
+  wire s1 = (cstate_i == 3'b001);
+  wire s2 = (cstate_i == 3'b010);
 
-	wire fn0 = ir_i[14:12] == 3'd0;
-	wire fn1 = ir_i[14:12] == 3'd1;
-	wire fn2 = ir_i[14:12] == 3'd2;
-	wire fn3 = ir_i[14:12] == 3'd3;
-	wire fn4 = ir_i[14:12] == 3'd4;
-	wire fn5 = ir_i[14:12] == 3'd5;
-	wire fn6 = ir_i[14:12] == 3'd6;
-	wire fn7 = ir_i[14:12] == 3'd7;
+  // Although defined as "registers", these are really asynchronous
+  // outputs.
 
-	wire row0 = ir_i[6:5] == 2'd0;
-	wire row1 = ir_i[6:5] == 2'd1;
-	wire row2 = ir_i[6:5] == 2'd2;
-	wire row3 = ir_i[6:5] == 2'd3;
+  reg defined_o;
+  reg alub_imm6i_o;
+  reg alub_imm12_o;
+  reg [2:0] nstate_o;
+  reg ra_ir1_o;
+  reg ra_ird_o;
+  reg alua_rf_o;
+  reg rf_alu_o;
+  reg [3:0] rmask_o;
+  reg cflag_1_o;
+  reg sum_en_o;
+  reg and_en_o;
+  reg xor_en_o;
+  reg lsh_en_o;
+  reg rsh_en_o;
+  reg invB_en_o;
+  reg ltu_en_o;
+  reg lts_en_o;
+  reg sx32_en_o;
 
-	wire col0 = ir_i[4:2] == 3'd0;
-	wire col1 = ir_i[4:2] == 3'd1;
-	wire col2 = ir_i[4:2] == 3'd2;
-	wire col3 = ir_i[4:2] == 3'd3;
-	wire col4 = ir_i[4:2] == 3'd4;
-	wire col5 = ir_i[4:2] == 3'd5;
-	wire col6 = ir_i[4:2] == 3'd6;
-	wire col7 = ir_i[4:2] == 3'd7;
+  always @(*) begin
+    case (ir_i[1:0])
+      2'b11: begin
+      case (ir_i[4:2])
+      3'b100, 3'b110: begin
+        case (ir_i[6:5])
+        2'b00: begin    // OP-IMM
+          case (ir_i[14:12])
+          3'b000: begin // ADDI
+            defined_o <= 1;
+            alub_imm6i_o <= 0;
+            alub_imm12_o <= s0;
+            ra_ir1_o <= s1;
+            alua_rf_o <= s2;
+            ra_ird_o <= s2;
+            rf_alu_o <= s2;
+            rmask_o <= {4{s2}};
+            cflag_1_o <= 0;
+            sum_en_o <= s2;
+            and_en_o <= 0;
+            xor_en_o <= 0;
+            lsh_en_o <= 0;
+            rsh_en_o <= 0;
+            invB_en_o <= 0;
+            ltu_en_o <= 0;
+            lts_en_o <= 0;
+            sx32_en_o <= s2 & ir_i[3];
+          end
+          3'b001: begin
+            case({ir_i[31:25], ir_i[3]})
+            {7'b0000000, 1'b0}, {7'b0000001, 1'b0}: begin	// SLLI
+              defined_o <= 1;
+              alub_imm6i_o <= s0;
+              alub_imm12_o <= 0;
+              ra_ir1_o <= s1;
+              alua_rf_o <= s2;
+              ra_ird_o <= s2;
+              rf_alu_o <= s2;
+              rmask_o <= {4{s2}};
+              cflag_1_o <= 0;
+              sum_en_o <= 0;
+              and_en_o <= 0;
+              xor_en_o <= 0;
+              lsh_en_o <= s2;
+              rsh_en_o <= 0;
+              invB_en_o <= 0;
+              ltu_en_o <= 0;
+              lts_en_o <= 0;
+              sx32_en_o <= 0;
+            end
+            {7'b0000000, 1'b1}: begin	// SLLIW
+              defined_o <= 1;
+              alub_imm6i_o <= s0;
+              alub_imm12_o <= 0;
+              ra_ir1_o <= s1;
+              alua_rf_o <= s2;
+              ra_ird_o <= s2;
+              rf_alu_o <= s2;
+              rmask_o <= {4{s2}};
+              cflag_1_o <= 0;
+              sum_en_o <= 0;
+              and_en_o <= 0;
+              xor_en_o <= 0;
+              lsh_en_o <= s2;
+              rsh_en_o <= 0;
+              invB_en_o <= 0;
+              ltu_en_o <= 0;
+              lts_en_o <= 0;
+              sx32_en_o <= s2;
+            end
+            default: begin
+              defined_o <= 0;
+              alub_imm6i_o <= 0;
+              alub_imm12_o <= 0;
+              ra_ir1_o <= 0;
+              alua_rf_o <= 0;
+              ra_ird_o <= 0;
+              rf_alu_o <= 0;
+              rmask_o <= 4'b0000;
+              cflag_1_o <= 0;
+              sum_en_o <= 0;
+              and_en_o <= 0;
+              xor_en_o <= 0;
+              lsh_en_o <= 0;
+              rsh_en_o <= 0;
+              invB_en_o <= 0;
+              ltu_en_o <= 0;
+              lts_en_o <= 0;
+              sx32_en_o <= s2 & ir_i[3];
+            end
+            endcase
+          end
+          3'b010: begin // SLTI
+            defined_o <= 1;
+            alub_imm6i_o <= 0;
+            alub_imm12_o <= s0;
+            ra_ir1_o <= s1;
+            alua_rf_o <= s2;
+            ra_ird_o <= s2;
+            rf_alu_o <= s2;
+            rmask_o <= {4{s2}};
+            cflag_1_o <= s2;
+            sum_en_o <= 0;
+            and_en_o <= 0;
+            xor_en_o <= 0;
+            lsh_en_o <= 0;
+            rsh_en_o <= 0;
+            invB_en_o <= s2;
+            ltu_en_o <= 0;
+            lts_en_o <= s2;
+            sx32_en_o <= s2 & ir_i[3];
+          end
+          3'b011: begin // SLTUI
+            defined_o <= 1;
+            alub_imm6i_o <= 0;
+            alub_imm12_o <= s0;
+            ra_ir1_o <= s1;
+            alua_rf_o <= s2;
+            ra_ird_o <= s2;
+            rf_alu_o <= s2;
+            rmask_o <= {4{s2}};
+            cflag_1_o <= s2;
+            sum_en_o <= 0;
+            and_en_o <= 0;
+            xor_en_o <= 0;
+            lsh_en_o <= 0;
+            rsh_en_o <= 0;
+            invB_en_o <= s2;
+            ltu_en_o <= s2;
+            lts_en_o <= 0;
+            sx32_en_o <= s2 & ir_i[3];
+          end
+          3'b100: begin // XORI
+            defined_o <= 1;
+            alub_imm6i_o <= 0;
+            alub_imm12_o <= s0;
+            ra_ir1_o <= s1;
+            alua_rf_o <= s2;
+            ra_ird_o <= s2;
+            rf_alu_o <= s2;
+            rmask_o <= {4{s2}};
+            cflag_1_o <= 0;
+            sum_en_o <= 0;
+            and_en_o <= 0;
+            xor_en_o <= s2;
+            lsh_en_o <= 0;
+            rsh_en_o <= 0;
+            invB_en_o <= 0;
+            ltu_en_o <= 0;
+            lts_en_o <= 0;
+            sx32_en_o <= s2 & ir_i[3];
+          end
+          3'b101: begin
+            case({ir_i[31:25], ir_i[3]})
+            {7'b0000000, 1'b0}, {7'b0000001, 1'b0}: begin	// SRLI
+              defined_o <= 1;
+              alub_imm6i_o <= s0;
+              alub_imm12_o <= 0;
+              ra_ir1_o <= s1;
+              alua_rf_o <= s2;
+              ra_ird_o <= s2;
+              rf_alu_o <= s2;
+              rmask_o <= {4{s2}};
+              cflag_1_o <= 0;
+              sum_en_o <= 0;
+              and_en_o <= 0;
+              xor_en_o <= 0;
+              lsh_en_o <= 0;
+              rsh_en_o <= s2;
+              invB_en_o <= 0;
+              ltu_en_o <= 0;
+              lts_en_o <= 0;
+              sx32_en_o <= 0;
+            end
+            {7'b0000000, 1'b1}: begin	// SRLIW
+              defined_o <= 1;
+              alub_imm6i_o <= s0;
+              alub_imm12_o <= 0;
+              ra_ir1_o <= s1;
+              alua_rf_o <= s2;
+              ra_ird_o <= s2;
+              rf_alu_o <= s2;
+              rmask_o <= {4{s2}};
+              cflag_1_o <= 0;
+              sum_en_o <= 0;
+              and_en_o <= 0;
+              xor_en_o <= 0;
+              lsh_en_o <= 0;
+              rsh_en_o <= s2;
+              invB_en_o <= 0;
+              ltu_en_o <= 0;
+              lts_en_o <= 0;
+              sx32_en_o <= s2;
+            end
+            {7'b0100000, 1'b0}, {7'b0100001, 1'b0}: begin	// SRAI
+              defined_o <= 1;
+              alub_imm6i_o <= s0;
+              alub_imm12_o <= 0;
+              ra_ir1_o <= s1;
+              alua_rf_o <= s2;
+              ra_ird_o <= s2;
+              rf_alu_o <= s2;
+              rmask_o <= {4{s2}};
+              cflag_1_o <= s2;
+              sum_en_o <= 0;
+              and_en_o <= 0;
+              xor_en_o <= 0;
+              lsh_en_o <= 0;
+              rsh_en_o <= s2;
+              invB_en_o <= 0;
+              ltu_en_o <= 0;
+              lts_en_o <= 0;
+              sx32_en_o <= 0;
+            end
+            {7'b0100000, 1'b1}: begin	// SRAIW
+              defined_o <= 1;
+              alub_imm6i_o <= s0;
+              alub_imm12_o <= 0;
+              ra_ir1_o <= s1;
+              alua_rf_o <= s2;
+              ra_ird_o <= s2;
+              rf_alu_o <= s2;
+              rmask_o <= {4{s2}};
+              cflag_1_o <= s2;
+              sum_en_o <= 0;
+              and_en_o <= 0;
+              xor_en_o <= 0;
+              lsh_en_o <= 0;
+              rsh_en_o <= s2;
+              invB_en_o <= 0;
+              ltu_en_o <= 0;
+              lts_en_o <= 0;
+              sx32_en_o <= s2;
+            end
+            default: begin
+              defined_o <= 0;
+              alub_imm6i_o <= 0;
+              alub_imm12_o <= 0;
+              ra_ir1_o <= 0;
+              alua_rf_o <= 0;
+              ra_ird_o <= 0;
+              rf_alu_o <= 0;
+              rmask_o <= 4'b0000;
+              cflag_1_o <= 0;
+              sum_en_o <= 0;
+              and_en_o <= 0;
+              xor_en_o <= 0;
+              lsh_en_o <= 0;
+              rsh_en_o <= 0;
+              invB_en_o <= 0;
+              ltu_en_o <= 0;
+              lts_en_o <= 0;
+              sx32_en_o <= 0;
+            end
+            endcase
+          end
+          3'b110: begin // ORI
+            defined_o <= 1;
+            alub_imm6i_o <= 0;
+            alub_imm12_o <= s0;
+            ra_ir1_o <= s1;
+            alua_rf_o <= s2;
+            ra_ird_o <= s2;
+            rf_alu_o <= s2;
+            rmask_o <= {4{s2}};
+            cflag_1_o <= 0;
+            sum_en_o <= 0;
+            and_en_o <= s2;
+            xor_en_o <= s2;
+            lsh_en_o <= 0;
+            rsh_en_o <= 0;
+            invB_en_o <= 0;
+            ltu_en_o <= 0;
+            lts_en_o <= 0;
+            sx32_en_o <= s2 & ir_i[3];
+          end
+          3'b111: begin // ANDI
+            defined_o <= 1;
+            alub_imm6i_o <= 0;
+            alub_imm12_o <= s0;
+            ra_ir1_o <= s1;
+            alua_rf_o <= s2;
+            ra_ird_o <= s2;
+            rf_alu_o <= s2;
+            rmask_o <= {4{s2}};
+            cflag_1_o <= 0;
+            sum_en_o <= 0;
+            and_en_o <= s2;
+            xor_en_o <= 0;
+            lsh_en_o <= 0;
+            rsh_en_o <= 0;
+            invB_en_o <= 0;
+            ltu_en_o <= 0;
+            lts_en_o <= 0;
+            sx32_en_o <= s2 & ir_i[3];
+          end
+          endcase
 
-	wire inst32b = ir_i[1:0] == 2'b11;
-
-	wire is_not_shift = ~(fn1 | fn5);
-	wire is_shift_left_64 = ~ir_i[3] & fn1 & ir_i[31:26] == 6'b000000;
-	wire is_shift_right_64 = ~ir_i[3] & fn5 & ir_i[31] == 0 & ir_i[29:26] == 4'b0000;
-	wire is_shift_left_32 = ir_i[3] & fn1 & ir_i[31:25] == 7'b0000000;
-	wire is_shift_right_32 = ir_i[3] & fn5 & ir_i[31] == 0 & ir_i[29:25] == 5'b00000;
-	wire is_shift_left = is_shift_left_32 | is_shift_left_64;
-	wire is_shift_right = is_shift_right_32 | is_shift_right_64;
-	
-	wire is_op_imm = row0 & col4 & inst32b & (is_not_shift | is_shift_left | is_shift_right);
-	wire is_op_imm32 = row0 & col6 & inst32b & (is_not_shift | is_shift_left | is_shift_right);
-
-	assign defined_o = is_op_imm | is_op_imm32;
-	assign alua_rf_o = s2;
-	assign alub_imm12_o = s0 & (is_op_imm | is_op_imm32) & (is_not_shift);
-	assign alub_imm6i_o = s0 & (is_op_imm | is_op_imm32) & (~is_not_shift);
-	assign ra_ir1_o = s1;
-	assign ra_ird_o = s2;
-	assign rf_alu_o = s2;
-	assign rmask_o = s2 ? 4'b1111 : 4'b0000;
-	assign cflag_1_o = s2 & (fn2 | fn3 | (fn5 & ir_i[30]));
-	assign sum_en_o = s2 & fn0;
-	assign and_en_o = s2 & (fn6 | fn7);
-	assign xor_en_o = s2 & (fn4 | fn6);
-	assign invB_en_o = s2 & (fn2 | fn3);
-	assign lsh_en_o = s2 & fn1;
-	assign rsh_en_o = s2 & fn5;
-	assign ltu_en_o = s2 & fn3;
-	assign lts_en_o = s2 & fn2;
-	assign sx32_en_o = s2 & ir_i[3];
-
-	assign nstate_o =
-		s0 ? 1 :
-		s1 ? 2 :
-		s2 ? 3 : 3;
+          case (cstate_i)
+          3'd0: begin
+            nstate_o <= 1;
+          end
+          3'd1: begin
+            nstate_o <= 2;
+          end
+          3'd2: begin
+            nstate_o <= 3;
+          end
+          3'd3: begin
+            nstate_o <= 3;
+          end
+          default: begin
+            nstate_o <= cstate_i;
+          end
+          endcase
+        end
+        default: begin
+          defined_o <= 0;
+          alub_imm6i_o <= 0;
+          alub_imm12_o <= 0;
+          ra_ir1_o <= 0;
+          alua_rf_o <= 0;
+          ra_ird_o <= 0;
+          rf_alu_o <= 0;
+          rmask_o <= 4'b0000;
+          cflag_1_o <= 0;
+          sum_en_o <= 0;
+          and_en_o <= 0;
+          xor_en_o <= 0;
+          lsh_en_o <= 0;
+          rsh_en_o <= 0;
+          invB_en_o <= 0;
+          ltu_en_o <= 0;
+          lts_en_o <= 0;
+          sx32_en_o <= 0;
+        end
+        endcase
+      end
+      default: begin
+        defined_o <= 0;
+        alub_imm6i_o <= 0;
+        alub_imm12_o <= 0;
+        ra_ir1_o <= 0;
+        alua_rf_o <= 0;
+        ra_ird_o <= 0;
+        rf_alu_o <= 0;
+        rmask_o <= 4'b0000;
+        cflag_1_o <= 0;
+        sum_en_o <= 0;
+        and_en_o <= 0;
+        xor_en_o <= 0;
+        lsh_en_o <= 0;
+        rsh_en_o <= 0;
+        invB_en_o <= 0;
+        ltu_en_o <= 0;
+        lts_en_o <= 0;
+        sx32_en_o <= 0;
+      end
+      endcase
+    end
+    default: begin
+      defined_o <= 0;
+      alub_imm6i_o <= 0;
+      alub_imm12_o <= 0;
+      ra_ir1_o <= 0;
+      alua_rf_o <= 0;
+      ra_ird_o <= 0;
+      rf_alu_o <= 0;
+      rmask_o <= 4'b0000;
+      cflag_1_o <= 0;
+      sum_en_o <= 0;
+      and_en_o <= 0;
+      xor_en_o <= 0;
+      lsh_en_o <= 0;
+      rsh_en_o <= 0;
+      invB_en_o <= 0;
+      ltu_en_o <= 0;
+      lts_en_o <= 0;
+      sx32_en_o <= 0;
+    end
+    endcase
+  end
 endmodule
 
