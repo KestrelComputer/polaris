dc466751e03483ac182455e35f2bc3e4432cd0aa
Samuel A. Falvo II <kc5tja@arrl.net>
2016-11-25 16:03:05 -0800

    Include build instructions

--- docs/example/example.asm
+++ docs/example/example.asm
@@ -1,9 +1,23 @@
-		adv	$F00, $CC
+; The following program was built and converted into Verilog hex-dump
+; format with the following commands.  Your assembler may use slightly
+; different syntax or command-line options.
+;
+; Note that the call to xxd and awk must sit on one line or it won't work.
+;
+; a from example.asm to example.bin
+; xxd -g 8 -c 8 example.bin |                                    \
+;   awk -e '{print substr($2,15,2)substr($2,13,2)substr($2,11,2) \
+;   substr($2,9,2)substr($2,7,2)substr($2,5,2)substr($2,3,2)     \
+;   substr($2,1,2);}' >example.hex
+;
 
+		adv	$F00, $CC
 		jal	1, main		; Call our main program, setting
 					; X1 to point at our string.
+
 		byte	"Hello world!",13,10,0
 		align	4
+
 main:		jal	2, writeStr	; Write the string the console.
 		csrrwi	0, 2, $0FF	; End the simulation successfully.
 
